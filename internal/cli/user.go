4c01288244047a3a1298d5ba5b88f8c0363a1100
damiankopyto <damian.kopyto@intel.com>
2025-10-30 10:45:21 +0000

    Backup branch to keep KC user related changes

--- internal/cli/user.go
+++ internal/cli/user.go
@@ -6,6 +6,7 @@ package cli
 import (
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/Nerzal/gocloak/v13"
 	"github.com/open-edge-platform/cli/pkg/auth"
@@ -34,27 +35,71 @@ var UserHeader = fmt.Sprintf("\n%s\t%s", "Name", "Status")
 // Prints OS Profiles in tabular format
 func printUsers(writer io.Writer, users []*gocloak.User, verbose bool) {
 	if verbose {
-		fmt.Fprintf(writer, "\n%s\t%s\t%s\n", "Name", "Status", "Description")
+		fmt.Fprintf(writer, "\n%s\t%s\t%s\n", "Name", "Status", "User ID")
 	}
 
 	for _, user := range users {
 		if !verbose {
-			fmt.Fprintf(writer, "%s\t%s\n", *user.Username, *user.Enabled)
+			fmt.Fprintf(writer, "%s\t%v\n", *user.Username, *user.Enabled)
 		} else {
-			fmt.Fprintf(writer, "%s\t%s\t%s\n", *user.Username, *user.Enabled, *user.ID)
+			fmt.Fprintf(writer, "%s\t%v\t%s\n", *user.Username, *user.Enabled, *user.ID)
 		}
 	}
 }
 
 // Prints output details of OS Profiles
-func printUser(writer io.Writer, name string, project *tenancy.GetprojectProject) {
+func printUser(writer io.Writer, user *gocloak.User, groups []*gocloak.Group) {
 
-	_, _ = fmt.Fprintf(writer, "Name: \t%s\n", name)
-	_, _ = fmt.Fprintf(writer, "Description: \t%s\n", *project.Spec.Description)
-	_, _ = fmt.Fprintf(writer, "Status: \t%s\n", *project.Status.ProjectStatus.StatusIndicator)
-	_, _ = fmt.Fprintf(writer, "Status message: \t%s\n", *project.Status.ProjectStatus.Message)
-	_, _ = fmt.Fprintf(writer, "UID: \t%s\n\n", *project.Status.ProjectStatus.UID)
+	email, id, fname, lname, enabled, emailVerified, createdAt := "", "", "", "", false, false, int64(0)
+	if user.Email != nil {
+		email = *user.Email
+	}
+	if user.ID != nil {
+		id = *user.ID
+	}
+	if user.FirstName != nil {
+		fname = *user.FirstName
+	}
+	if user.LastName != nil {
+		lname = *user.LastName
+	}
+	if user.Enabled != nil {
+		enabled = *user.Enabled
+	}
+	if user.EmailVerified != nil {
+		emailVerified = *user.EmailVerified
+	}
+	if user.CreatedTimestamp != nil {
+		createdAt = *user.CreatedTimestamp
+	}
+	_, _ = fmt.Fprintf(writer, "Name: \t%s\n", *user.Username)
+	_, _ = fmt.Fprintf(writer, "Email: \t%s\n", email)
+	_, _ = fmt.Fprintf(writer, "ID: \t%s\n", id)
+	_, _ = fmt.Fprintf(writer, "First Name: \t%s\n", fname)
+	_, _ = fmt.Fprintf(writer, "Last Name: \t%s\n", lname)
+	_, _ = fmt.Fprintf(writer, "Status (Enabled): \t%v\n", enabled)
+	_, _ = fmt.Fprintf(writer, "Email verified?: \t%v\n", emailVerified)
+	// Convert Unix timestamp to readable time format
+	if user.CreatedTimestamp != nil {
+		createdTime := time.Unix(0, createdAt*int64(time.Millisecond))
+		_, _ = fmt.Fprintf(writer, "Created at: \t%s\n", createdTime.Format("2006-01-02 15:04:05 MST"))
+	} else {
+		_, _ = fmt.Fprintf(writer, "Created at: \t%s\n", "N/A")
+	}
 
+	// Print each group in human-readable format
+	if groups != nil && len(groups) > 0 {
+		_, _ = fmt.Fprintf(writer, "Groups:\n")
+		for i, group := range groups {
+			groupName := "Unknown"
+			if group.Name != nil {
+				groupName = *group.Name
+			}
+			_, _ = fmt.Fprintf(writer, "  [%d]: \t%s\n", i+1, groupName)
+		}
+	} else {
+		_, _ = fmt.Fprintf(writer, "Groups: \t%s\n", "None")
+	}
 }
 
 func getGetUserCommand() *cobra.Command {
@@ -105,31 +150,44 @@ func getDeleteUserCommand() *cobra.Command {
 	return cmd
 }
 
-// Gets specific Cloud Init configuration bu resource ID
+// Gets specific user
 func runGetUserCommand(cmd *cobra.Command, args []string) error {
 
 	name := args[0]
-	writer, verbose := getOutputContext(cmd)
-	ctx, userClient, _, err := TenancyFactory(cmd)
+	writer, _ := getOutputContext(cmd)
+	ctx, userClient, _, err := getKCServiceContext(cmd)
 	if err != nil {
 		return err
 	}
-
-	resp, err := userClient.GETV1ProjectsProjectProjectWithResponse(ctx, name, auth.AddAuthHeader)
+	params := gocloak.GetUsersParams{
+		Username: &name,
+	}
+	token, err := auth.GetAccessToken(ctx)
+	if err != nil {
+		return err
+	}
+	users, err := userClient.GetUsers(ctx, token, "master", params)
 	if err != nil {
 		return processError(err)
 	}
 
-	if proceed, err := processResponse(resp.HTTPResponse, resp.Body, writer, verbose,
-		"", "error getting projects"); !proceed {
-		return err
+	//check precise username
+	if len(users) == 0 || *users[0].Username != name {
+		return fmt.Errorf("user %s not found", name)
 	}
 
-	printProject(writer, name, resp.JSON200)
+	gparams := gocloak.GetGroupsParams{}
+
+	groups, err := userClient.GetUserGroups(ctx, token, "master", *users[0].ID, gparams)
+	if err != nil {
+		return processError(err)
+	}
+
+	printUser(writer, users[0], groups)
 	return writer.Flush()
 }
 
-// Lists all Cloud Init configurations - retrieves all configurations and displays selected information in tabular format
+// Lists all users
 func runListUserCommand(cmd *cobra.Command, _ []string) error {
 	writer, verbose := getOutputContext(cmd)
 
@@ -152,7 +210,7 @@ func runListUserCommand(cmd *cobra.Command, _ []string) error {
 	return writer.Flush()
 }
 
-// Creates Project
+// Creates User
 func runCreateUserCommand(cmd *cobra.Command, args []string) error {
 	name := args[0]
 
@@ -177,7 +235,7 @@ func runCreateUserCommand(cmd *cobra.Command, args []string) error {
 	return checkResponse(resp.HTTPResponse, resp.Body, fmt.Sprintf("error while creating projet"))
 }
 
-// Deletes Project - checks if a project already exists and then deletes it if it does
+// Deletes User - checks if a user already exists and then deletes it if it does
 func runDeleteUserCommand(cmd *cobra.Command, args []string) error {
 
 	name := args[0]
