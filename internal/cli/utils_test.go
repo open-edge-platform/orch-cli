b6c0d3b1113d053975e9ccff00b6105a9ffed0b9
damiankopyto <damian.kopyto@intel.com>
2026-01-14 15:37:22 +0000

    Missing utils unit test

--- internal/cli/utils_test.go
+++ internal/cli/utils_test.go
@@ -171,6 +171,11 @@ func TestGetServiceContexts(t *testing.T) {
 	//nolint:dogsled
 	_, _, err = getTenancyServiceContext(cmd)
 	assert.NoError(t, err)
+
+	// Tenancy
+	//nolint:dogsled
+	_, _, err = getOrchestratorServiceContext(cmd)
+	assert.NoError(t, err)
 }
 
 func TestCheckResponseGRPC(t *testing.T) {
